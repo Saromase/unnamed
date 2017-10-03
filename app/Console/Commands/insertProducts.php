a816d5837402387cd4de6ad1331ea711223c46e4
saromase <seite.romain@hotmail.fr>
2017-10-03 23:40:23 +0200

    Add command to insert in Product

---
+++ app/Console/Commands/insertProducts.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use Illuminate\Database\Facades\DB;
+use \App\Products;
+
+class insertProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:products:insert';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Insert Products on Products base';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+ public function handle()
+    {
+        Products::insert([
+           [
+               'name' => 'water',
+               'price' => '50'
+           ],
+            [
+               'name' => 'wood',
+               'price' => '10'
+           ],
+            [
+               'name' => 'rock',
+               'price' => '75'
+           ]
+            
+        ]);
+        echo "Vous avez bien ajouté les données dans la table \n";
+    }
+}
