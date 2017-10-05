159a5552aa063a08cdec4c28bd2b10d127c318f5
saromase <seite.romain@hotmail.fr>
2017-10-05 22:27:15 +0200

    add database migration stockage

---
+++ database/migrations/2017_10_05_202350_stockage.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Stockage extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stockage', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('length');
+            $table->integer('price');
+            $table->engine = 'InnoDB';
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stockage');
+    }
+}
