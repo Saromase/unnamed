5357cf2e332097e86d7cb49bbbd14d978317e335
Alexandre Besnier <alexandre-besnierh@gmail.com>
2017-10-06 14:33:52 +0200

    Layout storage page

--- resources/views/storage.blade.php
+++ resources/views/storage.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app') @section('content')
-<div class="container">
+<div id="storage" class="container">
     @foreach($storage as $local)@endforeach
     <h1 class="<text-center></text-center>">Bienvenue dans votre {{ $local->name }}</h1>
-    <div class="main">
+    <!-- <div class="main">
         <img src="/img/storage/room.jpg" alt="Room" class="img-responsive">
-    </div>
+    </div> -->
     <div class="test">
-        <h1 class="text-center">{{ $local->name  }}</h1>
+        <h2 class="text-center">{{ $local->name  }}</h2>
         <p> Taille du stockage : {{ $local->length  }}</p>
         <p>Name Products : Number</p>
         <p>Name Products : Number</p>
