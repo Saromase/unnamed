060b4be3bf1e22b80632ed27ae81de904f949b17
saromase <seite.romain@hotmail.fr>
2017-10-06 22:54:36 +0200

    update market blade

--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    @foreach($products as $product)
-        <h1>{{ $product->name }}</h1>
-        <p>{{ $product->price }}</p>
-    @endforeach
+<div id="storage" class="container">
+    <div class="test">
+            @foreach($products as $product)
+            <h2>{{ $product->name }}</h2>
+            <p>{{ $product->price }}</p>
+        @endforeach
+    </div>
 </div>
 @endsection
