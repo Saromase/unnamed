e4e10172e87eabae11f2ff00ab17e09807158bbc
saromase <seite.romain@hotmail.fr>
2017-10-10 23:50:42 +0200

    cleary synthaxe for storage controller

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -16,28 +16,32 @@ class StorageController extends Controller
 
     public function displayStorages() {
         $userId = Auth::user()->id;
-        $storageId = UserStats::where('user_id', '=', "$userId")->select('user_storage')->get();
-        $inventoryId = UserStats::where('user_id', '=', "$userId")->select('user_inventory')->get();
-        foreach ($storageId as $idS){}
-        $playerStorage = Storage::where('id', '=', "$idS->user_storage")->get();
-        foreach ($inventoryId as $idI){}
-        $inventory = Inventory::where('id', '=', "$userId")->get();
-        \Log::info($inventory);
-        foreach ($inventory as $inv){}
-        \Log::info($inv->water);
-        $productsName = Products::select('name')->get();
-        $productsNumber = [];
-        foreach ($inventory as $material) {
-          foreach ($productsName as $productName) {
-            $productName = $productName->name;
-            array_push($productsNumber, $material->$productName);
-          }
-        }
+        
+        // On recupere l'id du storage que possède l'utilisateur
+        $storageId = UserStats::where('user_id', '=', "$userId")
+            ->select('user_storage')
+            ->get()
+            ->first();
+        
+        // On recupere l'ensemble des informations liée à ce storage qu'on envoie à la vue
+        $playerStorage = Storage::where('id', '=', "$storageId->user_storage")
+            ->get()
+            ->first();
+        
+        // On recupere l'id de l'inventaire que possède l'utilisateur
+        $inventoryId = UserStats::where('user_id', '=', "$userId")
+            ->select('user_inventory')
+            ->get()
+            ->first();
+        
+        // On recupere l'ensemble des informations liée à l'inventaire de l'utilisateur
+        $inventory = Inventory::where('id', '=', "$inventoryId->user_inventory")
+            ->get();
+        
+        
         return view('storage', [
             'storage' => $playerStorage,
-            'inventory' => $inventory,
-            'productsName' => $productsName,
-            'productsNumber' => $productsNumber
+            'inventory' => $inventory
         ]);
     }
 
--- resources/views/storage.blade.php
+++ resources/views/storage.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app') @section('content')
 <div id="storage" class="container">
-    @foreach($storage as $local)@endforeach
-    <h1 class="<text-center></text-center>">Bienvenue dans votre {{ $local->name }}</h1>
+    <h1 class="<text-center></text-center>">Bienvenue dans votre {{ $storage->name }}</h1>
 
     <div class="test">
-        <h2 class="text-center">{{ $local->name }}</h2>
-        <h3 class="text-center"> Taille du stockage : {{ $local->length }}</h3>
-        @for($i = 0; $i < count($productsName); $i++)
-            <div>
-                <h2>{{ $productsName[$i]->name }}</h2>
-                <p>{{ $productsNumber[$i] }}</p>
-            </div>
-        @endfor
+        <h2 class="text-center">{{ $storage->name }}</h2>
+        <h3 class="text-center"> Taille du stockage : {{ $storage->length }}</h3>
+        <!--@foreach ($inventory as $product)
+        <div>
+            <h1>{{$product->name}}</h1>
+        </div>
+        @endforeach-->
     </div>
 
 
