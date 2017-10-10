6baa521e60e1dcd798a8576fe6d450f444ef5b12
François Mathieu <maps_red@yahoo.fr>
2017-10-10 23:04:03 +0200

    Update method name
    Signed-off-by: François Mathieu <maps_red@yahoo.fr>

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -24,7 +24,7 @@ class Model extends BaseModel
     public function __call($method, $params)
     {
         $type = substr($method, 0, 3);
-        if (null != $var = $this->from_camel_case(lcfirst(substr($method, 3)))) {
+        if (null != $var = $this->fromCamelCase(lcfirst(substr($method, 3)))) {
             if ($type == "get") {
                 return $this->__get($var);
             }elseif ($type == "set") {
@@ -51,7 +51,7 @@ class Model extends BaseModel
      * @param $input
      * @return string
      */
-    public function from_camel_case($input)
+    public function fromCamelCase($input)
     {
         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
         $ret = $matches[0];
