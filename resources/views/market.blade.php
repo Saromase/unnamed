8cf1b6845ce7b10a6cb621d41f6c5034dc23f34c
Alexandre Besnier <alexandreh.besnier@gmail.com>
2017-10-11 12:28:08 +0200

    Update design Market and Storage

--- resources/views/market.blade.php
+++ resources/views/market.blade.php
@@ -12,22 +12,19 @@
 </div>
 @endisset
 
-<div id="storage" class="container">
-    <div class="test">
+<div id="market" class="container main">
+    <h1>Bienvenue au marché</h1>
+    <div class="row flex-container">
         @foreach($products as $product)
-           <div class="product">
-               <div class="product-picture"></div>
+            <div class="flex-item">
+                <div class="product-picture"></div>
                 <h3>{{ $product->name }}</h3>
                 <p>{{ $product->price }} €</p>
-                    <form action='/market/buy/{{$product->id}}' method="get">
+                <form action='/market/buy/{{$product->id}}' method="get">
                     {{ csrf_field() }}
-                     <button type="submit">Acheter une unité</button>
-                   
+                    <button type="submit">Acheter une unité</button>
                 </form>
-           </div>
+            </div>
         @endforeach
     </div>
 </div>
-@endsection
-
-
--- resources/views/storage.blade.php
+++ resources/views/storage.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.app') @section('content')
-<div id="storage" class="container">
+<div id="storage" class="container main">
     @foreach($storage as $local)@endforeach
     <h1 class="<text-center></text-center>">Bienvenue dans votre {{ $local->name }}</h1>
 
-    <div class="test">
+    <div class="row">
         <h2 class="text-center">{{ $local->name }}</h2>
         <h3 class="text-center"> Taille du stockage : {{ $local->length }}</h3>
-        @for($i = 0; $i < count($productsName); $i++)
-            <div>
-                <h2>{{ $productsName[$i]->name }}</h2>
-                <p>{{ $productsNumber[$i] }}</p>
-            </div>
-        @endfor
+        <div>
+            <table class="table">
+                <thead>
+                    <th>Nom</th>
+                    <th>Nombre</th>
+                </thead>
+                <tbody>
+                    @for($i = 0; $i < count($productsName); $i++)
+                        <tr>
+                            <td>{{ $productsName[$i]->name }}</td>
+                            <td>{{ $productsNumber[$i] }}</td>
+                        </tr>
+                    @endfor
+                </tbody>
+            </table>
+        </div>
     </div>
 
 
