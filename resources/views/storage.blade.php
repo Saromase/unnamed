b1b501bcd917d9ead44548c6bb383492f3178a14
Alexandre Besnier <alexandreh.besnier@gmail.com>
2017-10-13 20:52:57 +0200

    Update page Storage

--- resources/views/storage.blade.php
+++ resources/views/storage.blade.php
@@ -2,18 +2,19 @@
 @section('content')
 
     <!-- Error -->
-    @isset ($warning)
+    @if (session('warning'))
         <div class="alert alert-danger">
-            {{ $failure}}
+            {{ session('warning') }}
         </div>
-    @endisset
+    @endif
+
 
     <!-- Success -->
-    @isset ($success)
+    @if (session('success'))
         <div class="alert alert-success">
-            {{ $success }}
+            {{ session('success') }}
         </div>
-    @endisset
+    @endif
 
     <div id="storage" class="container main">
         <h1>Bienvenue dans votre {{ $storage->name }}</h1>
@@ -23,7 +24,34 @@
             <h3 class="text-center"> Taille du stockage : {{ $storage->length }}</h3>
 
             <!-- Button upgrade storage -->
-            <button class="btn btn-info" type="button" data-toggle="modal" data-target="#storageUpgrade">Plus d'espace ?</button>
+            @isset($buttonUpgrade)
+                <button class="btn btn-info" type="button" data-toggle="modal" data-target="#storageUpgrade">Plus d'espace ?</button>
+            @endisset
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="storageUpgrade" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Voulez-vous agrandir votre {{ $storage->name }} ?</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p>Votre argent : {{ $playerMoney }}</p>
+                            <p>Prix : {{ $upgradePrice }}</p>
+                            <form action="/storage/storageUpgrade" method="get">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-default">Accepter</button>
+                            </form>
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
 
             <!-- List items -->
             <div>
@@ -50,27 +78,3 @@
     </div>
 
 @endsection
-
-<!-- Modal -->
-<div class="modal fade" id="storageUpgrade" role="dialog">
-    <div class="modal-dialog">
-
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Voulez-vous agrandir votre {{ $storage->name }} ?</h4>
-            </div>
-            <div class="modal-body">
-                <p>Votre argent : {{ $playerMoney }}</p>
-                <p>Prix : {{ $upgradePrice }}</p>
-                <form action="/storage/storageUpgrade" method="get">
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-default">Accepter</button>
-                </form>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-
-    </div>
-</div>
