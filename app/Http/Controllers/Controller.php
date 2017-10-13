e5087824aed7a57d7461000a13aa18a3b32c9938
François MATHIEU <francois.mathieu@livexp.fr>
2017-10-13 16:38:19 +0200

    Add addFlash helper for Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,10 +9,33 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /** @var Request $request */
+    private $request;
+
+    /**
+     * @param Request $request
+     * @return Controller
+     */
+    public function setRequest(Request $request)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
 
     /**
      * @return User|Authenticatable|null
@@ -20,5 +43,23 @@ class Controller extends BaseController
     public function getUser()
     {
         return Auth::user();
+    }
+
+    /**
+     * @param $type
+     * @param $message
+     * @param Request|null $request
+     * @return Controller
+     */
+    public function addFlash($type, $message, Request $request = null)
+    {
+        $this->request = $request instanceof Request ? $request : $this->request;
+        if (!$this->getRequest() instanceof Request) {
+            throw new \RuntimeException('Request not defined, use setRequest first');
+        }
+
+        $this->request->session()->flash($type, $message);
+
+        return $this;
     }
 }
