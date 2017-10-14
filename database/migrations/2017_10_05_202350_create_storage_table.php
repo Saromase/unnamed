c01fd7c5289d5243d54ee1a73914d33af346134f
saromase <seite.romain@hotmail.fr>
2017-10-14 20:13:12 +0200

    Fix name

--- database/migrations/2017_10_05_202350_create_storage_table.php
+++ database/migrations/2017_10_05_202350_create_storage_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStorageClass extends Migration
+class CreateStorageTable extends Migration
 {
     /**
      * Run the migrations.
