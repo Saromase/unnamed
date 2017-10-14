91d170a3a69be0fa5050a24f6984a467347c36a8
saromase <seite.romain@hotmail.fr>
2017-10-14 20:51:25 +0200

    Fix product insert

--- app/Console/Commands/InsertProducts.php
+++ app/Console/Commands/InsertProducts.php
@@ -34,76 +34,39 @@ class InsertProducts extends Command
      */
     public function handle()
     {
-        Products::insert([
-            [
-                'name' => 'Eau',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Pierre',
-                'price' => '15',
-                'min_price' => '10',
-                'max_price' => '20'
-            ],
-            [
-                'name' => 'Gaz naturel',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Petrole',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Aluminium',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Or',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Cuivre',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Fer',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Sable',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Charbon',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
-            [
-                'name' => 'Bois',
-                'price' => '5',
-                'min_price' => '0',
-                'max_price' => '10'
-            ],
+        $temp = [];
+        $final = [];
+        $total = 0;
 
-        ]);
-
-        echo "Vous avez bien ajouté les données dans la table \n";
+        $name = ['Eau', 'Pierre', 'Gaz Naturel', 'Petrole', 'Aluminium', 'Or', 'Cuivre', 'Fer', 'Sable', 'Charbon', 'Bois'];
+        
+        for ($i = 0; $i < count($name); $i++){
+            $quantity = rand(0,15000);
+            $regeneration = $quantity / 100;
+            $total += $quantity;
+            array_push($temp, [
+               'name' => $name[$i],
+                'quantity' => $quantity,
+                'regeneration' => round($regeneration),
+            ]);
+        }
+        
+        for ($j = 0; $j < count($name); $j++){
+            $quantity = $temp[$j]['quantity'];
+            $regeneration = $temp[$j]['regeneration'];
+            $percentage = ($quantity / $total) * 100;
+            $price = $total / $quantity * 25;
+            array_push($final, [
+               'name' => $name[$j],
+                'quantity' => $quantity,
+                'regeneration' => round($regeneration),
+                'percentage' => round($percentage),
+                'price' => round($price),
+                'supply_demand' => rand(-100, 100),
+            ]);
+        }
+        
+        Products::insert($final);
+        
     }
 }
