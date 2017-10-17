f4ba325aa1634b968160a9711a5738d7c27112e4
Alexandre Besnier <alexandreh.besnier@gmail.com>
2017-10-17 22:34:04 +0200

    Update sass Admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,81 +1,74 @@
 @extends('layouts.app') @section('content')
-<div class="container">
+<nav class="navbar-vertical">
+    <ul class="nav navbar">
+        <li><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i> Produits</a></li>
+        <li><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i> Utilisateurs</a></li>
+        <li><a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i> Inventaires</a></li>
+    </ul>
+    <button id="reduce" type="button">
+      <i class="fa fa-minus" aria-hidden="true"></i>
+      <i class="fa fa-plus" aria-hidden="true"></i>
+    </button>
+</nav>
+
+<div id="admin" class="main">
     <!-- Error -->
-    <div class="row">
-        @if (session('warning'))
-        <div class="alert alert-danger">
-            {{ session('warning') }}
-        </div>
-        @endif
+    @if (session('warning'))
+    <div class="alert alert-danger">
+        {{ session('warning') }}
+    </div>
+    @endif
 
-        <!-- Success -->
-        @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-        @endif
-        <div class="alert" id="alert" style="display: none;">
-            {{-- Ajax use --}}
-        </div>
+    <!-- Success -->
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+    <div class="alert" id="alert" style="display: none;">
+        {{-- Ajax use --}}
     </div>
 
+    <div class="jumbottron">
+      <h1>Administrations</h1>
+      <p>Du texte en vrac <span>et un span</span>.</p>
 
-    <div class="panel-group" id="accordion">
-        <div class="panel panel-info">
-           <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Bienvenue</a>
-                </h4>
-            </div>
-            <div id="collapse1" class="panel-collapse collapse in">
-                <div class="panel-body">
-                    Bienvenue administrateur
-                </div>
-            </div>
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Produits</a>
-                </h4>
-            </div>
-            <div id="collapse2" class="panel-collapse collapse">
-                <div class="panel-body">
-                    <!-- Tableau des items-->
-                    <div class="items">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Nom</th>
-                                    <th>Quantité</th>
-                                    <th>Quantité %</th>
-                                    <th>Prix Médian</th>
-                                    <th>Prix</th>
-                                    <th>Demande</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($products as $product)
-                                <tr>
-                                    <td>{{ $product->name }}</td>
-                                    <td>{{ $product->quantity }}</td>
-                                    <td>{{ $product->percentage }}</td>
-                                    <td>{{ $product->median_price }}</td>
-                                    <td id="{{$product->name}}Price">{{ $product->price }}</td>
-                                    @if ($product->supply_demand > 0) <td class="success" id="{{$product->name}}Demand"> 
-                                    @else <td class="danger" id="{{$product->name}}Demand"> 
-                                    @endif
-                                    {{ $product->supply_demand }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- Option -->
-                    <button type="button" class="btn btn-primary" id="refreshProductsPrice">Raffraichir</button>
-                </div>
-            </div>
-            
-        </div>
+      <!-- Option -->
+      <div class="options">
+        <button type="button" class="btn btn-primary" id="refreshProductsPrice">Raffraichir</button>
+      </div>
     </div>
 
+    <!-- Tableau des items-->
+    <div class="items">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Quantité</th>
+                    <th>Quantité %</th>
+                    <th>Prix Médian</th>
+                    <th>Prix</th>
+                    <th>Demande</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($products as $product)
+                    <tr>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->quantity }}</td>
+                        <td>{{ $product->percentage }}</td>
+                        <td>{{ $product->median_price }}</td>
+                        <td id="{{$product->name}}Price">{{ $product->price }}</td>
+                        @if ($product->supply_demand > 0) <td class="success" id="{{$product->name}}Demand">
+                        @else <td class="danger" id="{{$product->name}}Demand">
+                        @endif {{ $product->supply_demand }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-    @endsection
+</div>
+
+@endsection
