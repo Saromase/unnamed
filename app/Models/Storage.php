db32321affe1db2d2df5800d0b659bf185064d9f
Romain <seite.romain@yahoo.com>
2017-10-18 14:57:54 +0200

    Regenerate Doc for models

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -11,23 +11,23 @@ use Illuminate\Database\Eloquent\Collection;
  * @property string $name
  * @property int $length
  * @property int $price
- * @method static Collection|Storage[] findBy(array $value)
- * @method static Collection|Storage[] findByLength($value)
- * @method static Collection|Storage[] findByName($value)
- * @method static Collection|Storage[] findByPrice($value)
- * @method static Storage findOneBy(array $value)
- * @method static Storage findOneById($value)
- * @method static Storage findOneByLength($value)
- * @method static Storage findOneByName($value)
- * @method static Storage findOneByPrice($value)
+ * @method static \Illuminate\Database\Eloquent\Collection|\App\Models\Storage[] findBy(array $value)
+ * @method static \Illuminate\Database\Eloquent\Collection|\App\Models\Storage[] findByLength($value)
+ * @method static \Illuminate\Database\Eloquent\Collection|\App\Models\Storage[] findByName($value)
+ * @method static \Illuminate\Database\Eloquent\Collection|\App\Models\Storage[] findByPrice($value)
+ * @method static \App\Models\Storage findOneBy(array $value)
+ * @method static \App\Models\Storage findOneById($value)
+ * @method static \App\Models\Storage findOneByLength($value)
+ * @method static \App\Models\Storage findOneByName($value)
+ * @method static \App\Models\Storage findOneByPrice($value)
  * @method integer getId()
  * @method integer getLength()
  * @method string getName()
  * @method integer getPrice()
- * @method Storage setId($value)
- * @method Storage setLength($value)
- * @method Storage setName($value)
- * @method Storage setPrice($value)
+ * @method \App\Models\Storage setId($value)
+ * @method \App\Models\Storage setLength($value)
+ * @method \App\Models\Storage setName($value)
+ * @method \App\Models\Storage setPrice($value)
  * @mixin \Eloquent
  */
 class Storage extends CustomModel
