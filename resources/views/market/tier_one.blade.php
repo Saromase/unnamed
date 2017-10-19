2935da5e6c2f8773a08d83588358778a0fab3df1
Romain <seite.romain@yahoo.com>
2017-10-19 09:31:29 +0200

    Refactoring Blade

--- resources/views/market/tier_one.blade.php
+++ resources/views/market/tier_one.blade.php
@@ -1,17 +1,4 @@
-@extends('layouts.app')
-@section('test')
-    <nav id="navbar-vertical">
-        <ul class="nav navbar">
-            <li><a href="{{ route('marketTierOne') }}"><i class="fa fa-chevron-right" aria-hidden="true"></i> Tier 1 </a></li>
-            <li><a href="{{ route('marketTierTwo') }}"><i class="fa fa-chevron-right" aria-hidden="true"></i> Tier 2</a></li>
-            <li><a href="{{ route('marketFactory') }}"><i class="fa fa-chevron-right" aria-hidden="true"></i> Factory</a></li>
-        </ul>
-        <button id="reduce" type="button">
-            <i class="fa fa-minus" aria-hidden="true"></i>
-            <i class="fa fa-plus" aria-hidden="true"></i>
-        </button>
-    </nav>
-@endsection
+@extends('layouts.market')
 @section('content')
     <div id="admin" class="main">
         <!-- Error -->
