4ea8e4632b230c0ede1ccead9c8cf85c10bb5935
Alexandre Besnier <alexandreh.besnier@gmail.com>
2017-10-20 16:34:32 +0200

    Add products

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -13,7 +13,7 @@ class ProductsTableSeeder extends Seeder
         $final = [];
         $total = 0;
 
-        $name = ['Eau', 'Pierre', 'Gaz', 'Petrole', 'Aluminium', 'Or', 'Cuivre', 'Fer', 'Sable', 'Charbon', 'Bois'];
+        $name = ['eau', 'pierre', 'gaz', 'pétrole', 'bauxite', 'minerai d\'or', 'minerai de cuivre', 'minerai de fer', 'sable', 'charbon', 'bois', 'lingot de fer', 'lingot de cuivre', 'lingot d\'or', 'lingot d\'aluminium', 'silex', 'carbone compresser', 'anneau de fer', 'anneau de cuivre', 'anneau d\'aluminium', 'anneau d\'or', 'diamant', 'planche', 'clous', 'diamant taillé', 'meuble', 'armature', 'anneau d\'or serti d\'un diamant', 'verre', 'vitrine en verre'];
         $color = ['#00c2d0', '#4e4e55', '#e4e4e4', '#1f1f1f', '#bf675a', '#ffd600', '#df9169', '#897474', '#f3ad48', '#445055', '#b77400'];
 
         for ($i = 0; $i < count($name); $i++) {
